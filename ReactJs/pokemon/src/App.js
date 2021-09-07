df49a32a97e515143f16e6a57ebf8110c311cf5f
cdceballor1 <cdceballor@gmail.com>
2021-09-07 11:17:12 -0500

    Component to call

--- ReactJs/pokemon/src/App.js
+++ ReactJs/pokemon/src/App.js
@@ -1,33 +1,10 @@
-import React, {useEffect, useState} from 'react'
-import { Button, ListItem } from "@material-ui/core"
-//import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import ConsumingApi from './components/ConsumingApi'
 
 function App() {
-  const url = "https://pokeapi.co/api/v2/pokemon/"
-  const [todos, setTodos] = useState()
-  const fetchApi = async () => {
-    const response = await fetch(url)
-    const resposeJSON = await response.json()
-    setTodos(resposeJSON)
-    console.log(todos)
-  }
-  useEffect(()=> {
-    fetchApi()
-  })
   return (
-    <div className="App">
-      <Button href = "https://pokeapi.co/" variant="contained" color="primary">Pokemon Api Consumer</Button>
-      <ul>
-        {! todos ? "Cargando ... ": 
-        todos.results.map((todo, index)=> {
-          return <li><ListItem primary="Inbox">{todos.results[index].name}</ListItem></li>
-          
-        })
-        }
-
-      </ul>   
-    </div>
+    <ConsumingApi />
   );
 }
 
